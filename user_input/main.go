bd225417848bdfc5826c2a0c8ede30ae63ba3827
stungnet <stungcodes@proton.me>
2023-02-24 21:16:00 +1100

    Added comments to input and write.

--- user_input/main.go
+++ user_input/main.go
@@ -1,19 +1,35 @@
-package main
+package main //Main package to that will create executable
 
+// Imports from standard library
 import (
-	"fmt"
-	"strings"
+	"fmt" // Format and print functions for outputting data to console.
+	"os" // Operating system functions.
+	"strings" // String manipulation functions.
 )
 
+// Main function is our entry point for the program where our code begins execution when executed.
 func main(){
-	var input string
+	var input string // Our string which user input will be stored.
+	
+	// The beginning of our for loop
 	for {
+		// Ask user question with fmt.Printf, then take the input with fmt.Scanln.
 		fmt.Printf("Please enter 'a' or 'b': ")
-		fmt.Scan(&input)
+		fmt.Scanln(&input)
+
+		// If statement will check that user has not enter just blank. This will only check the first entry not subsequent.
+		if len(strings.TrimSpace(input)) == 0{
+			fmt.Println("You didn't enter anything? ¯(°_o)/¯")
+			os.Exit(1)
+		}
+
+		// We take that use input and convert it to lowercase.
 		userInputLower := strings.ToLower(input)
 
+		// Let the user know what they entered.
 		fmt.Println("You entered", userInputLower)
 
+		// If block will check to see if "a" or "b" if neither user will be asked again until it is "a" or "b".
 		if userInputLower == "a"{
 			fmt.Println("You listened!")	
 			break
--- write/main.go
+++ write/main.go
@@ -1,24 +1,32 @@
-package main
+package main // Main package that will create executable
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+	"bufio" // Buffered input and output opertations; Improve reading and writing.
+	"fmt" // Format and print functions for outputting data to console.
+	"os" // Operating system functions
 )
 
+// Main function is entry point for the program, where code begins execution when executed.
 func main(){
-	var filename string
-	fmt.Printf("Give your new .txt file a name: ")
-	fmt.Scan(&filename)	
-	filename += ".txt" // add .txt extension to user input.
+	var filename string // String to store new filename
+	fmt.Printf("Give your new .txt file a name: ") // Prompt user.
+	fmt.Scan(&filename)	// Take user input.
 
+	// Here we add .txt to the users inputted filename.
+	filename += ".txt" 
+	
+	// Creating filename.txt
 	os.Create(filename)
-	fmt.Printf("%v created!", filename)
+	fmt.Printf("%v created!", filename) // Let user know it has being created.
 
+	// Prompt user to enter text to our new filename.txt
 	fmt.Println("What do you want to write to the file? ")
-	scanner := bufio.NewReader(os.Stdin)
+	// Create a new bufio.Reader that reads from os.Stdin, which is our users input.
+	scanner := bufio.NewReader(os.Stdin) 
+	// Using the `scanner` we created just before we ReadString until the end of the line and assign it to contents.
 	contents, _ := scanner.ReadString('\n')
 	
+	// Convert the input to byes to write it to the file.
 	data := []byte(contents)
 	os.WriteFile(filename, data, 0664)
 }
